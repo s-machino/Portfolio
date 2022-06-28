dbcfb097c867b540c7df6053974e923dd8117e9f
s-machino <hamu3103hamu@gmail.com>
2022-06-29 00:49:13 +0900

    aboutページ文言修正

--- pages/about.vue
+++ pages/about.vue
@@ -5,17 +5,18 @@
       <div class="sec-1 _en">
         <h2 class="title-1">Profile</h2>
         <p>Name：Satomi Machino</p>
-        <p>Job：Web Designer / Front-end Engineer</p>
+        <p>Job：Web Designer / Front-end Developer</p>
       </div>
       <div class="sec-2">
         <h2 class="title-1">Overview</h2>
         <p>
           Webデザイナー、フロントエンドエンジニアとして活動をしております。<br />
           デザインツール：Photoshop、Illustrator、Adobe XD<br />
-          プログラミング言語：HTML、CSS（Sass）、JavaScript（ES6・JQuery・Vue.js・React）、Typescript、WordPress<br />
+          プログラミング言語：HTML、CSS（Sass）、JavaScript（ES6・JQuery・Vue.js・React）、Typescript<br />
+          WordPressやmicroCMSなどにも対応させていただいております。<br />
           Webデザイナー、フロントエンドエンジニアとして東京都内の制作会社に3年在籍後フリーランスに。<br />
-          現在は業務委託のフロントエンジニアとして事業会社様にアサイン。<br />
-          制作会社在籍時は部署のチームリーダーやディレクションも経験。<br />
+          制作会社在籍時は部署のチームリーダーやディレクションも経験いたしました。<br />
+          現在では業務委託のフロントエンジニアとして事業会社様にアサイン。<br />
           現時点で担当した案件数は200件以上。個人での案件受託もしております。
         </p>
       </div>
