7fc472a113d3e0439990537d82ab787dc0b067ee
s-machino <hamu3103hamu@gmail.com>
2022-08-03 05:06:20 +0900

    updated file

--- pages/about.vue
+++ pages/about.vue
@@ -11,13 +11,19 @@
         <h2 class="title-1">Overview</h2>
         <p>
           Webデザイナー、フロントエンドエンジニアとして活動をしております。<br />
-          デザインツール：Photoshop、Illustrator、Adobe XD<br />
-          プログラミング言語：HTML、CSS（Sass）、JavaScript（ES6・JQuery・Vue.js・React）、Typescript<br />
+          デザインツール：Photoshop、Illustrator、Adobe XDなど。<br />
+          プログラミング言語：HTML、CSS（Sass）、JavaScript（React・Vanilla(ES6)・JQuery）、Typescript<br />
           WordPressやmicroCMSなどにも対応させていただいております。<br />
-          Webデザイナー、フロントエンドエンジニアとして東京都内の制作会社に3年在籍後フリーランスに。<br />
-          制作会社在籍時は部署のチームリーダーやディレクションも経験いたしました。<br />
-          現在では業務委託のフロントエンジニアとして事業会社様にアサイン。<br />
-          現時点で担当した案件数は200件以上。個人での案件受託もしております。
+        </p>
+      </div>
+      <div class="sec-3">
+        <h2 class="title-1">Summary</h2>
+        <p>
+          Webデザイナー、フロントのコーダーとしてWeb制作会社に3年間在籍し、Webサイト制作に従事。<br />
+          在籍時は部署のチームリーダーやディレクションなども経験いたしました。<br />
+          その後フリーランスに転換、業務委託のフロントエンドエンジニアとして、Webアプリケーションのフロント開発に従事。<br />
+          現在では、自社開発を行う事業会社に在籍し、フロントエンドエンジニアとして更なるスキルアップ、キャリアアップを目指しています。<br />
+          現時点で担当した案件数は200件以上。Webアプリケーション開発やWebサイト制作など、個人での案件受託もしております。
         </p>
       </div>
     </div>
